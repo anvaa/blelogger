e36f673b174c719c353f5d08bf5a105db5df9feb
anvaa <anvaa@outlook.com>
2022-11-30 20:07:22 +0100

    Update dothejob.py

--- dothejob.py
+++ dothejob.py
@@ -6,16 +6,16 @@ import os
 def getTimeNow():
     return datetime.datetime.now()
 
-#Return formated datetime string
+#Return formated datetime string 
 def getNowShortFormated():
     return datetime.datetime.today().strftime("%d%m%y%H%m%S")
 
 #Return formated datetime string
-def getNowLongFormated():
-    return datetime.datetime.now().strftime("%d%m%y%H%m%s")
+def getNowSQLFormated():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%m:%S")
 
 def formatDateTime(formatThis):
-    return formatThis.strftime("%d.%m.%y %H:%m:%s")
+    return formatThis.strftime("%Y.%m.%d %H:%m:%s")
 
 def runToTime(runfor=0.0):
     from datetime import timedelta
@@ -24,24 +24,28 @@ def runToTime(runfor=0.0):
         print(f"Running for {runfor} minutes.")
     return newRunTotime
 
-def checkJsonFileFolder(jfilename):
-    p=os.getcwd()+'/jsonf/'
+def checkJsonFileFolder(jname):
+    if checkFolderEx():
+      print(f'Writing -> {jname}_n.json')
+    else:
+      print("Not a folder!")  
+
+def checkCsvFileFolder(cname):          
+    if checkFolderEx():
+      print(f'Appending -> {cname}.csv') 
+    else:
+      print("Not a folder!")  
+        
+def checkFolderEx():
+    p = os.getcwd()+"/logg/"  
     if not os.path.exists(p):
         os.mkdir(p)
-        print(f"First time run: Made JSON folder: {p}")
-        return False
-    print(f'Writing -> {jfilename}_n.json')
-
-
-def checkCsvFileFolder(cfilename):  # check if folder exists
-    p=os.getcwd()+"/csvf/"          # program workdir + filedir
-    if not os.path.exists(p):       # if not exists,
-        os.mkdir(p)                 # make dir
-        print(f"First time run: Made CSV folder: {p}")  # user info
-    print(f'Appending -> {cfilename}.csv')              # user info
+        print(f"First time run: Made <logg> folder: {p}")
+        return True
+    else: return True
 
 #Format and write json/csv data from ble-scaning
-def writeData(devices, fname="wdname", writejson=0, writecsv=0, jcount=0):
+def writeData(devices, fname, writejson=0, writecsv=0, jcount=0):
 
     devices_m = []
 
@@ -56,19 +60,19 @@ def writeData(devices, fname="wdname", writejson=0, writecsv=0, jcount=0):
                 name = str(value)
 
         # add device addr, addType and rssi to devices_m
-        devices_m.append({'time': getNowLongFormated(), 'addr': dev.addr, 'rssi': dev.rssi, 'name': name})
+        devices_m.append({'time': getNowSQLFormated(), 'addr': dev.addr, 'rssi': dev.rssi, 'name': name})
 
     print(f"Scan {jcount}: {len(devices_m)} devices found.")
     devices_m.sort(key=lambda x: x["rssi"], reverse=True) #Sort in signalstrength(db)
     json_devices = json.dumps(devices_m)
 
     if writejson > 0:
-        newjf = f"jsonf/{fname}_{jcount}.json"
+        newjf = f"logg/{fname}_{jcount}.json"
         with open(newjf, "w") as outfile:
             outfile.write(json_devices)
 
     if writecsv > 0:
-        newcf = f"csvf/{fname}.csv"
+        newcf = f"logg/{fname}.csv"
         data_file = open(newcf, 'a')
         csv_writer = csv.writer(data_file)
 
